f7815c5fcd4e0cfa16aceaf3bdc61d8ce3024dec
Tobejizzle <71057635+Tobejizzle@users.noreply.github.com>
2026-04-28 00:13:35 -0400

    remove debugging line

--- packages/utils/src/format.ts
+++ packages/utils/src/format.ts
@@ -37,7 +37,5 @@ export function capitalizeSplit(str: string): string {
       .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
       .join(" ");
 
-  console.log(result);
-
   return result;
 }
